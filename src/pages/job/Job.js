0fdb8a7da695be439844dcd9674ab26663108d56
C L <happycindy26@gmail.com>
2021-10-11 19:38:49 +1100

    refactoring job.js and skill.js

--- src/pages/job/Job.js
+++ src/pages/job/Job.js
@@ -4,12 +4,14 @@ import './style.css';
 
 const Job = (props) => {
     const jobs = (props.location && props.location.jobs) || null;
+    const skill = (props.location && props.location.skill) || null;
     return (
         <div>
-            <h1>Skill used In following roles</h1>
+            <h1>{skill}</h1>
+            <h1>Skill used in following roles</h1>
             <div>
                 <ul>
-                {jobs && jobs.map((job, index) => <li key={index}>{job.title}</li>)}
+                    {jobs && jobs.map((job, index) => <li key={index}>{job.title}</li>)}
                 </ul>
             </div>
         </div>
--- src/pages/skill/Skill.js
+++ src/pages/skill/Skill.js
@@ -1,30 +1,22 @@
-import { useState } from 'react';
 import skills from '../../data/skillsData';
 //import Card from "../../components/card/Card";
 import "./style.css";
 
 
 function Skill(props) {
-  const [job, setJob] = useState();
   const handleClick = (id) => { 
     const result =  skills.find((skill)=> skill.id === id);
-    const jobs = result.occupation.map(j => j.title);
-    setJob(jobs);
     props.history.push({
       pathname: "/job",
-      jobs: result.occupation
+      jobs: result.occupation,
+      skill: result.skill
     });      
   }
   return (
     <div className='skill'>
-      <h5>Choose the skills you have</h5>
+      <h5>Choose the skill you have</h5>
       <div className='container'>
-        {skills.map((skill) => {
-          return (
-          <button onClick={() => handleClick(skill.id)}>{skill.skill}
-          </button>
-          );
-        })}
+        {skills.map((skill) => <button onClick={() => handleClick(skill.id)}>{skill.skill}</button>)}
       </div>       
     </div> 
   );
