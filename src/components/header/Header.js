23021f7eafa99d8de3164ded77b0a0f390846233
Valerie Clarke <havl.webdev@gmail.com>
2021-10-30 20:10:25 +1100

    Progress on CSS based on Hanna's designs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,19 @@
-import logo from '../../images/logo-chavel.png';
+import logo from '../../images/CHAVL_Logo_V2.png';
 import './Header.css';
 
 const Header = () => (
     <header>
-        <img className="logo" src={logo} alt={logo} />
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Occupation Profile</li>
-        </ul>
+        <div class="container">
+            <section id="blue_bar"></section>
+
+            <img className="logo" src={logo} alt={logo} />
+            
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Occupation Profile</li>
+            </ul>
+        </div>
     </header>
 )
 
