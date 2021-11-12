1f90b7f872cdb9369ea4f2a2cfe2a9fe3f51cfe4
Cindy <51303237+happycindy26@users.noreply.github.com>
2021-11-12 11:26:28 +1100

    added skill percent on job page

--- src/pages/job/Job.js
+++ src/pages/job/Job.js
@@ -6,23 +6,12 @@ const Job = (props) => {
     const skill = (props.location && props.location.skill) || null;
     return (
         <div id="job_wrapper">
-<<<<<<< HEAD
             <h1>{skill}</h1>
             <h1>Skill used in following roles</h1>
             <div>
                 <ul>
                     {jobs && jobs.map((job, index) => <li key={index}>{job.title}&nbsp;{job.Percent}</li>)}
                 </ul>
-=======
-            <div id="container">
-                <h1>{skill}</h1>
-                <h1>This skill is used in the following roles:</h1>
-                <div>
-                    <ul>
-                        {jobs && jobs.map((job, index) => <li key={index}>{job.title}</li>)}
-                    </ul>
-                </div>
->>>>>>> a771d0e344b03a856b07ee78d24bf0449acedf99
             </div>
         </div>
     );
